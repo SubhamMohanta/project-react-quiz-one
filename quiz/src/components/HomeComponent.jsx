e19b0890ee17dc3fa28f83089b12ae8387bb1855
SubhamMohanta <subhammohanta99@gmail.com>
2024-04-26 16:19:20 +0530

    Done

---
+++ quiz/src/components/HomeComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const HomeComponent = () => {
+    return (
+        <div id='home-parent'>
+            <h1>QUIZ</h1>
+            <button>Play</button>
+        </div>
+    )
+}
+
+export default HomeComponent;
---
+++ quiz/src/components/QuizComponent.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+const QuizComponent = () => {
+
+        return (
+            <div id="quiz-parent">
+                <div id="quiz-box">
+                    <div id="quiz-head">
+                        <div id="heading">Question</div>
+                        <div id="q-num">1 of 15</div>
+                        <div id="question">Which is the only mammal which can jump?</div>
+                    </div>
+                    <div id="options-div">
+                        <div className="opt-row">
+                            <div className="option">Dog</div>
+                            <div className="option">Elephant</div>
+                        </div>
+                        <div className="opt-row">
+                            <div className="option">Goat</div>
+                            <div className="option">Lion</div>
+                        </div>
+                    </div>
+                    <div id="buttons">
+                        <button className="prev">PREVIOUS</button>
+                        <button className="quit">QUIT</button>
+                        <button className="next">NEXT</button>
+                    </div>
+                </div>
+            </div>
+        );
+    
+}
+
+export  default QuizComponent;
